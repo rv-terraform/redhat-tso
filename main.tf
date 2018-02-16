982d527cd7d34ffe4774fb872fecf058ccc6996b
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 16:57:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_subnet" "myterraformsubnet" {
 resource "azurerm_public_ip" "myterraformpublicip" {
     name                         = "myPublicIP"
     location                     = "${var.location}"
-    resource_grodress_an 
+    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
     public_ip_address_allocation = "dynamic"
 
     tags {
