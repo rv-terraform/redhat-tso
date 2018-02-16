1ec91b1fb4d8591a043730a03d2790d345fadc44
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 16:56:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "myterraformgroup" {
 resource "azurerm_virtual_network" "myterraformnetwork" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
-    location    up_nameur
+    location            = "${var.location}"
     resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
 
     tags {
