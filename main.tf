95d59a7c12ad9387e04968aa0f1e8c1bed57faa5
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 17:00:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "azurerm_network_interface" "myterraformnic" {
     ip_configuration {
         name                          = "myNicConfiguration"
         subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
-        private_ip_address_allocation = "dynamic"blici }
+        private_ip_address_allocation = "dynamic"
         public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
     }
 
