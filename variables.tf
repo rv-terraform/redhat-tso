71128529041876c546bd8e2273fcf5ada23b4d2d
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 16:35:02 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable subscription_id {}
+variable tenant_id {}
+variable client_id {}
+variable client_secret {}
+variable location {
+    default = "Canada East"
+}
+variable resource_group_name {
+    default = "myRG1"
+}
+
+variable adminuser {
+    default = "venerari"
+}
+
+variable hostname {
+    default = "myVM"
+}
+
+provider "azurerm" {
+   subscription_id = "${var.subscription_id}"
+   tenant_id = "${var.tenant_id}"
+   client_id = "${var.client_id}"
+   client_secret = "${var.client_secret}"
+}
